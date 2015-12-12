0f203d972f9c39bf2895b4e2ebe16755b09f0f91
icuic <icuic@foxmail.com>
2015-12-12 23:00:13 +0800

    Frame finished
    Frame almost finished. :+1:

--- FireLight.asm
+++ FireLight.asm
@@ -87,9 +87,9 @@ PPECR 	EQU 	38CH 			;PORTE 口上拉电阻控制寄存器
 ;*****************************************************
 ;Bank0
 ;------------------------------------------------------------------
-AC_BAK 	EQU 	30H 			;AC 值备份寄存器
+AC_BAK 		EQU 	30H 		;AC 值备份寄存器
 
-SIMULATE_STA	EQU	31H		;bit0 = 1, 进入"模拟停电"状态
+SIMULATE_STA	EQU	31H		;bit0 = 1, 已进入"模拟停电"状态, bit0 = 0, 已由"模拟停电"状态退出
 					;bit1 = 1, 进入"手动月检"状态
 					;bit2 = 1, 进入"手动年检"状态
 					;bit3 = 1, 进入"关断应急输出"状态
@@ -101,11 +101,13 @@ ABNORMAL_STA	EQU	33H		;bit0 = 1, 当前处于"停电"或"模拟停电"状态
 
 ;--------------------------------------
 ; 用于TIMER 定时
-F_BUTTON	EQU	35H		;供按键检测使用。bit0=1, 496ms 到;
+F_496MS_1S	EQU	35H		;bit0 = 1, 496ms 到，供按键检测使用。
+					;bit1 = 1, 1s 到，用于计时应急时长。
+
 F_TIME 		EQU 	36H 		;bit0=1, 1s 到; bit1=1, 1月到; bit2=1, 1年到。
 
-CNT0 		EQU 	37H 		;CNT1,CNT0组成的8BIT数据达到125时，即Timer0产生125次中断后，表示1S计时已到
-CNT1 		EQU 	38H 		;所以，CNT1=07H, CNT0=0DH
+CNT0_8MS 	EQU 	37H	 	;CNT1_8MS, CNT0_8MS组成的8BIT数据达到125时，即Timer0产生125次中断后，表示1S计时已到
+CNT1_8MS 	EQU 	38H 		;所以，初始化CNT1_8MS=07H, CNT0_8MS=0DH
 
 SEC_CNT0	EQU	39H		;SEC_CNT0/1/2 以秒为单位计时
 SEC_CNT1	EQU	3AH		;当数值达到1小时，即3600(E10H)秒时，向HOUR_CNT0/1进位，自身清零。
@@ -119,13 +121,13 @@ MONTH_CNT	EQU	3FH		;MONTH_CNT0/1 以月为单位计时
 					;当数值达到1年时，即12(0CH)月时，，同时置F_TIME.2，自身清零。
 
 TEMP_SUM_CY	EQU	40H		;AD子程序参数临时变量
-FLAG_OCCUPIED	EQU	41H		;bit0-3=1时分别表示CHN0、1、6、7的转换结果(CHN0_FINAL_RET1等)正被前台使用,
+FLAG_OCCUPIED	EQU	41H		;bit0/1/2/3 = 1时分别表示CHN0、1、6、7的转换结果(CHN0_FINAL_RET1等)正被前台使用,
 					;此时ADC中断不能修改这些数据。	
 
 FLAG_TYPE	EQU	42H		;bit0 = 1表示已经完成灯具类型选择(PORTB.3/AN7),此位为1后不再需要对AN7进行AD采样
 
 
-CMP_MIN_PWR0	EQU	43H		;上电自检时，灯具电源最小电压值
+CMP_MIN_PWR0	EQU	43H		;上电自检时，灯具电源最小电压值.(1.396V -> 0x23B ->(丢弃最低2位) 0x8E)
 CMP_MIN_PWR1	EQU	44H		;
 
 CMP_TYPE00	EQU	45H		;灯具类型门限0
@@ -143,11 +145,10 @@ LIGHT_TYPE	EQU	4BH		;灯具类型
 					;=2, 镍镉电池，常亮型
 					;=3, 镍镉电池，常灭型
 
-CMP_SUPPLY0	EQU	4CH		;检测到电源电压小于此数值时，开始应急放电
+CMP_SUPPLY0	EQU	4CH		;检测到电源电压小于此数值时，开始应急放电.(1.115V -> 0x5B)
 CMP_SUPPLY1	EQU	4DH
 
-FLAG_EMERGENCY	EQU	4EH		;bit0 = 1表示已打开应急放电功能
-					;bit1 = 1表示1s已到
+;XXXXXX		EQU	4EH
 					
 CNT0_EMERGENCY	EQU	4FH		;对应急时长计时，单位s
 CNT1_EMERGENCY	EQU	50H
@@ -173,8 +174,22 @@ BAT_STATE	EQU	58H		;bit0 = 0, 表示充电回路未开路；bit0 = 1, 表示充电回路开路
 					;bit3 = 1, 表示电池电压过低，不能再继续应急放电了
 
 
+CMP_LIGHT0	EQU	59H		;供光源检测使用
+CMP_LIGHT1	EQU	5AH
+
+LIGHT_STATE	EQU	5BH		;bit0 = 1, 表示光源故障
 
 
+ALARM_STATE	EQU	5CH		;bit0 = 1, 表示电池故障(开路或短路)
+					;bit1 = 1, 表示光源故障(开路或短路)
+					;bit2 = 1, 表示自检放电时间不足
+
+
+CNT_LED_YELLOW	EQU	5FH		;计数器，供翻转黄灯输出使用，计时单位为328MS
+
+
+FLAG_SIMU_EMER	EQU	60H		;bit0 = 1, 表示在"模拟停电"状态下已经打开应急功能
+					;bit1 = 1表示1s已到
 
 ;按键相关寄存器
 DELAY_TIMER2	EQU	71H		;延时子程序使用
@@ -189,9 +204,12 @@ CNT1_496MS	EQU	77H
 BTN_PRE_STA	EQU	78H		;bit0储存上一次按键状态,0:按下,1:未按下
 BTN_PRESS_CNT	EQU	79H		;按键按下时长，单位为496ms
 
-CNT0_200MS	EQU	7AH		;用于定时200MS
-CNT1_200MS	EQU	7BH
-F_200MS		EQU	7CH		;每200ms将bit0 = 1
+CNT0_328MS	EQU	7AH		;用于定时328MS,供翻转LED用
+CNT1_328MS	EQU	7BH
+F_328MS		EQU	7CH		;每328ms将bit0 = 1
+
+CMP_RESUME0	EQU	4DH		;检测到电源电压大于此数值时，表示市电供电已恢复，应关闭应急.(1.296V -> 0x84)
+CMP_RESUME1	EQU	4EH
 
 ;Bank1(以下寄存器真实地址应加上80H)
 ;------------------------------------------------------------------
@@ -307,16 +325,16 @@ TIMER0_ISP:
 	STA 	AC_BAK,		00H 	;备份AC 值
 	ANDIM 	IRQ,		1011B 	;清TIMER0 中断请求标志
 
-J_200MS:
-	SBIM	CNT0_200MS,	01H
+J_328MS:
+	SBIM	CNT0_328MS,	01H
 	LDI	TBR,		00H
-	SBCM	CNT1_200MS
+	SBCM	CNT1_328MS
 	BC	J_496MS
 
-	LDI	CNT0_200MS,	08H
-	LDI	CNT1_200MS,	01H
+	LDI	CNT0_328MS,	08H	;8ms * 41 = 328ms
+	LDI	CNT1_328MS,	02H
 
-	ORIM	F_200MS,	0001B
+	ORIM	F_328MS,	0001B
 	
 J_496MS:
 	;--------------------------------------------------------------------------
@@ -328,20 +346,21 @@ J_496MS:
 	LDI 	CNT0_496MS,	0DH 	;重置496ms 计数器,496 = 8 * 62
 	LDI 	CNT1_496MS,	03H 	;重置496ms 计数器
 	
-	ORIM 	F_BUTTON,	0001B 	;设置 "496ms 到"标志
+	ORIM 	F_496MS,	0001B 	;设置 "496ms 到"标志
 
 J1MS:	
 	;--------------------------------------------------------------------------
-	SBIM 	CNT0,		01H	;每次Timer0中断产生后，将CNT0减1
-	BC 	TIMER0_ISP_END 		;CNT0仍大于0, 则结束ISP
-	SBIM	CNT1,		01H	;每次CNT0-1产生借位时，将CNT1减1
+	SBIM 	CNT0_8MS,	01H	;每次Timer0中断产生后，将CNT0_8MS减1
+	BC 	TIMER0_ISP_END 		;CNT0_8MS仍大于0, 则结束ISP
+	SBIM	CNT1_8MS,	01H	;每次CNT0_8MS-1产生借位时，将CNT1_8MS减1
 	BC	TIMER0_ISP_END
 	
-	LDI 	CNT0,		0CH 	;重置1s 计数器
-	LDI 	CNT1,		07H 	;重置1s 计数器
+	LDI 	CNT0_8MS,	0CH 	;重置1s 计数器
+	LDI 	CNT1_8MS,	07H 	;重置1s 计数器
 	
 	ORIM 	F_TIME,		0001B 	;设置 "1s 到"标志
-	ORIM	FLAG_EMERGENCY,	0010B	;为应急功能提供 "1s 到"标志
+	ORIM	F_496MS_1S,	0010B	;为应急功能提供 "1s 到"标志
+	ORIM	FLAG_SIMU_EMER,	0010B	;为手动自检提供 "1s 到"标志
 	;--------------------------------------------------------------------------
 	
 
@@ -745,11 +764,11 @@ SYSTEM_INITIAL:
 	LDI 	TL0,		06H
 	LDI 	TH0,		00H 	;设置中断时间为8ms
 
-	LDI	CNT0_200MS,	08H	;定时200ms
-	LDI	CNT1_200MS,	01H	;
+	LDI	CNT0_328MS,	08H	;定时328ms
+	LDI	CNT1_328MS,	02H	;
 
-	LDI 	CNT0,		0DH 	;定时1s
-	LDI 	CNT1,		07H 	;定时1s
+	LDI 	CNT0_8MS,	0DH 	;定时1s
+	LDI 	CNT1_8MS,	07H 	;定时1s
 	
 	LDI	SEC_CNT0,	0FH	;SEC_CNT0/1/2 初始化为E10H - 1，即3600 -1
 	LDI	SEC_CNT1,	00H
@@ -815,9 +834,15 @@ SYSTEM_INITIAL:
 	LDI	ABNORMAL_STA,	00H	;无任何异常状态
 
 	;门限值
-	LDI	CMP_MIN_PWR0,	0FH	;最小上电电压
-	LDI	CMP_MIN_PWR1,	0FH
+	LDI	CMP_MIN_PWR0,	0EH	;最小上电电压(1.396V)
+	LDI	CMP_MIN_PWR1,	08H
 
+	LDI	CMP_SUPPLY0,	0BH	;小于此电压，则开启应急功能(1.115V -> 0x5B)
+	LDI	CMP_SUPPLY1,	05H
+
+	LDI	CMP_RESUME0,	04H	;大于此电压，则由应急转入主电(1.296V -> 0x84)
+	LDI	CMP_RESUME1,	08H
+	
 	LDI	CMP_TYPE00,	00H	;灯具类型门限0
 	LDI	CMP_TYPE01,	00H
 
@@ -881,7 +906,9 @@ MAIN:
 	;LDI	PWMD02,		07H
 
 ;--------------------------------------------------------------------------------------
-;检查供电情况，如若停电，则打开应急功能。同时检查电池电压，如果电池耗尽，则停止应急
+;检查供电情况，如若停电，则打开应急功能。
+;同时检查电池电压，如果电池耗尽，则停止应急
+;如果市电供电恢复，则返回主电，关闭应急
 ;--------------------------------------------------------------------------------------
 CHK_PWR_SUPPLY:				;检查供电是否正常
 	ORIM	FLAG_OCCUPIED,	0100B	;锁定通道6最终结果
@@ -896,15 +923,16 @@ CHK_PWR_SUPPLY:				;检查供电是否正常
 	BNC	CHK_BATTERY		;如果供电正常,则检测电池电压
 
 	;驱动应急引脚
-	LDA	FLAG_EMERGENCY
+	LDA	ABNORMAL_STA
 	BA0	EMERGENCY_CNT		;已打开应急放电功能
-	ORIM	FLAG_EMERGENCY,	0001B	;置标志位，表明已打开应急放电功能
+	ORIM	ABNORMAL_STA,	0001B	;置标志位，表明已打开应急放电功能
 	CALL	EMERGENCY_ENABLE
 
 EMERGENCY_CNT:
-	ADI	FLAG_EMERGENCY,	0010B	;每秒检查一次
+	ADI	F_496MS_1S,	0010B	;每秒检查一次
 	BA1	EMERGENCY_WAIT
 
+	ANDIM	F_496MS_1S,	1101B	;清除1s标志位
 	SBI	CNT2_EMERGENCY,	08H	;如果CNT达到0x800，则表示已应急放电0x800s = 2048s > 30min
 	BC	EMERGENCY_BATTERY	;此时无需再对应急放电时长计时
 
@@ -924,7 +952,8 @@ EMERGENCY_BATTERY:			;检查电池是否已经耗尽
 
 	ANDIM	FLAG_OCCUPIED,	1101B	;释放对通道1最终结果的锁定
 
-	BC	EMERGENCY_DISABLE	;电池电压过低，停止应急
+	BC	EMERGENCY_WAIT
+	CALL	EMERGENCY_DISABLE	;电池电压过低，停止应急
 
 
 EMERGENCY_WAIT:
@@ -975,22 +1004,43 @@ BAT_NEED_CHARGE:
 CHK_BATTERY_END:
 	ANDIM	FLAG_OCCUPIED,	1101B	;释放对通道1最终结果的锁定
 
+;--------------------------------------------------------------------------------------
+;光源检测
+;--------------------------------------------------------------------------------------
+CHK_LIGHT:
+	LDI	TBR,		0101B	;将立即数0101B载入累加器A中
+	AND	LIGHT_TYPE		;BIT0\2为1，代表灯具为常亮型
+
+	BNZ	TYPE_ON			;为常亮型灯具
+	JMP	TYPE_OFF		;为常灭型灯具
+
+	
+TYPE_ON:
+	CALL	PROCESS_LIGHT		;检测光源，并设置相应标志位
+	JMP     CHK_LIGHT_END
+
+
+TYPE_OFF:
+	LDA	NORMAL_STA		;载入NORMAL_STA至累加器A
+	BNZ	CHK_LIGHT_END		;处于主电源正常供电状态，无需对光源进行检测
+	CALL	PROCESS_LIGHT		;检测光源，并设置相应标志位
+
+CHK_LIGHT_END:
+
 
 ;--------------------------------------------------------------------------------------
-;根据电池电压检测结果，做相应处理
+;故障提示
 ;--------------------------------------------------------------------------------------
+CHK_ALARM:
+	CALL	PROCESS_ALARM		;根据故障标志位，控制黄灯与红灯
+	
 
-
-
-
-
-
-
-
-
-
+;--------------------------------------------------------------------------------------
+;按键扫描
+;--------------------------------------------------------------------------------------
 KEY_CHK:
 	CALL	KEY_CHECK_PROCESS	;按键扫描，输出"模拟停电","手动月检","手动年检","关断应急输出"等标志位
+
 
 CHECK_PWR_CUT:
 	ADI	SIMULATE_STA,	0001B	;检查"模拟停电"标志位
@@ -1036,72 +1086,100 @@ HALTMODE:
 
 
 
-;***********************************************************
-; 检查灯具类型
-; 输入: CHN7_FINAL_RET1, CHN7_FINAL_RET2
-; 输出: LIGHT_TYPE
-;***********************************************************
-CHK_TYPE:
-	ORIM	FLAG_OCCUPIED,	1000B	;锁定通道7最终结果
 
-	LDI	CHN7_FINAL_RET1,01H	;和门限0比较
-	SUB	CMP_TYPE00
-	LDI	CHN7_FINAL_RET2,01H
-	SBC	CMP_TYPE01
-	BNC	LI_ON			;
-
-	LDI	CHN7_FINAL_RET1,01H	;和门限1比较
-	SUB	CMP_TYPE10
-	LDI	CHN7_FINAL_RET2,01H
-	SBC	CMP_TYPE11
-	BNC	LI_OFF			;
-
-	LDI	CHN7_FINAL_RET1,01H	;和门限2比较
-	SUB	CMP_TYPE20
-	LDI	CHN7_FINAL_RET2,01H
-	SBC	CMP_TYPE21
-	BNC	NI_ON			;
-
-NI_OFF:
-	LDI	LIGHT_TYPE,	03H
-	JMP	CHK_TYPE_END
-	
-LI_ON:
-	LDI	LIGHT_TYPE,	00H
-	JMP	CHK_TYPE_END
-	
-LI_OFF:
-	LDI	LIGHT_TYPE,	01H
-	JMP	CHK_TYPE_END
-	
-NI_ON:
-	LDI	LIGHT_TYPE,	02H
-	
-CHK_TYPE_END:
-	ANDIM	FLAG_OCCUPIED,	0111B	;释放对通道7最终结果的锁定
-	ORIM	FLAG_TYPE,	0001B	;不再对通道7进行采样
-	
-	RTNI
 
 
 ;***********************************************************
 ; 模拟停电处理部分
 ;***********************************************************
 SIM_PWR_CUT:
-	NOP
+	LDA	FLAG_SIMU_EMER
+	BA0	SIM_PWR_CUT_END		;在"模拟停电"状态下，已打开应急放电功能
+	
+	ORIM	FLAG_SIMU_EMER,	0001B	;置标志位，表明在"模拟停电"状态下,已打开应急放电功能
+	CALL	EMERGENCY_ENABLE
+
+SIM_PWR_CUT_END:	
 	RTNI
 
 ;***********************************************************
 ; 手动月检处理部分
 ;***********************************************************
 SIM_MONTH_CHK:
+	LDA	FLAG_SIMU_EMER
+	BA1	SIM_MONTH_CHK_END	;在"手动月检"状态下，已打开应急放电功能
+	
+	ORIM	FLAG_SIMU_EMER,	0010B	;置标志位，表明在"手动月检"状态下,已打开应急放电功能
+	CALL	EMERGENCY_ENABLE
 
+
+SIM_MONTH_CNT:
+	ADI	FLAG_SIMU_EMER,	0010B	;每秒检查一次
+	BA1	SIM_MONTH_CHK_END
+
+	SBI	CNT2_EMERGENCY,	08H	;如果CNT达到0x800，则表示已应急放电0x800s = 2048s > 30min
+	BC	SIM_MONTH_BATTERY	;此时无需再对应急放电时长计时
+
+	ADIM	CNT0_EMERGENCY,	01H
+	LDI	TBR,		00H
+	ADCM	CNT1_EMERGENCY
+	LDI	TBR,		00H
+	ADCM	CNT2_EMERGENCY
+
+SIM_MONTH_BATTERY:			;检查电池是否已经耗尽
+	ORIM	FLAG_OCCUPIED,	0010B	;锁定通道1最终结果
+
+	LDI	CHN1_FINAL_RET1,01H
+	SUB	CMP_EXIT_EMER0
+	LDI	CHN1_FINAL_RET2,01H
+	SBC	CMP_EXIT_EMER1
+
+	ANDIM	FLAG_OCCUPIED,	1101B	;释放对通道1最终结果的锁定
+
+	BC	SIM_MONTH_CHK_END
+	CALL	EMERGENCY_DISABLE	;电池电压过低，停止应急
+
+SIM_MONTH_CHK_END:
 	RTNI
 
 ;***********************************************************
 ; 手动年检处理部分
 ;***********************************************************
 SIM_YEAR_CHK:
+	LDA	FLAG_SIMU_EMER
+	BA1	SIM_MONTH_CHK_END	;在"手动年检"状态下，已打开应急放电功能
+	
+	ORIM	FLAG_SIMU_EMER,	0010B	;置标志位，表明在"手动年检"状态下,已打开应急放电功能
+	CALL	EMERGENCY_ENABLE
+
+
+SIM_YEAR_CNT:
+	ADI	FLAG_SIMU_EMER,	0010B	;每秒检查一次
+	BA1	SIM_YEAR_CHK_END
+
+	SBI	CNT2_EMERGENCY,	08H	;如果CNT达到0x800，则表示已应急放电0x800s = 2048s > 30min
+	BC	SIM_YEAR_BATTERY	;此时无需再对应急放电时长计时
+
+	ADIM	CNT0_EMERGENCY,	01H
+	LDI	TBR,		00H
+	ADCM	CNT1_EMERGENCY
+	LDI	TBR,		00H
+	ADCM	CNT2_EMERGENCY
+
+SIM_YEAR_BATTERY:			;检查电池是否已经耗尽
+	ORIM	FLAG_OCCUPIED,	0010B	;锁定通道1最终结果
+
+	LDI	CHN1_FINAL_RET1,01H
+	SUB	CMP_EXIT_EMER0
+	LDI	CHN1_FINAL_RET2,01H
+	SBC	CMP_EXIT_EMER1
+
+	ANDIM	FLAG_OCCUPIED,	1101B	;释放对通道1最终结果的锁定
+
+	BC	SIM_YEAR_CHK_END
+	CALL	EMERGENCY_DISABLE	;电池电压过低，停止应急
+
+SIM_YEAR_CHK_END:
 
 	RTNI
 
@@ -1120,7 +1198,7 @@ KEY_CHECK_PROCESS:
 	LDI 	PDCR,		1110B 	;设置PD.0 为输入，PD.3 为输出
 	
 KEY_CHECK:
-	ADI	F_BUTTON,	0001B	;检查496MS标志位
+	ADI	F_496MS,	0001B	;检查496MS标志位
 	BA0	NOT_CHECK
 
 	
@@ -1208,7 +1286,7 @@ KEY_CHECK_PROCESS_OVER: 		;按键扫描及处理结束，返回
 	ANDIM	TEMP,		0001H	;
 	STA	BTN_PRE_STA		;TEMP -> BTN_PRE_STA
 	
-	ANDIM	F_BUTTON,	1110B	;清496MS标志位
+	ANDIM	F_496MS,	1110B	;清496MS标志位
 	
 NOT_CHECK:	
 	RTNI
@@ -2393,6 +2471,54 @@ CAL_CHN7_ADCDATA_END:
 	RTNI
 
 ;***********************************************************
+; 检查灯具类型
+; 输入: CHN7_FINAL_RET1, CHN7_FINAL_RET2
+; 输出: LIGHT_TYPE
+;***********************************************************
+CHK_TYPE:
+	ORIM	FLAG_OCCUPIED,	1000B	;锁定通道7最终结果
+
+	LDI	CHN7_FINAL_RET1,01H	;和门限0比较
+	SUB	CMP_TYPE00
+	LDI	CHN7_FINAL_RET2,01H
+	SBC	CMP_TYPE01
+	BNC	LI_ON			;
+
+	LDI	CHN7_FINAL_RET1,01H	;和门限1比较
+	SUB	CMP_TYPE10
+	LDI	CHN7_FINAL_RET2,01H
+	SBC	CMP_TYPE11
+	BNC	LI_OFF			;
+
+	LDI	CHN7_FINAL_RET1,01H	;和门限2比较
+	SUB	CMP_TYPE20
+	LDI	CHN7_FINAL_RET2,01H
+	SBC	CMP_TYPE21
+	BNC	NI_ON			;
+
+NI_OFF:
+	LDI	LIGHT_TYPE,	03H
+	JMP	CHK_TYPE_END
+	
+LI_ON:
+	LDI	LIGHT_TYPE,	00H
+	JMP	CHK_TYPE_END
+	
+LI_OFF:
+	LDI	LIGHT_TYPE,	01H
+	JMP	CHK_TYPE_END
+	
+NI_ON:
+	LDI	LIGHT_TYPE,	02H
+	
+CHK_TYPE_END:
+	ANDIM	FLAG_OCCUPIED,	0111B	;释放对通道7最终结果的锁定
+	ORIM	FLAG_TYPE,	0001B	;不再对通道7进行采样
+	
+	RTNI
+
+
+;***********************************************************
 ; 通过PWM0输出频率为32KHZ，占空比为50%的方波，以此驱动应急电路
 ;***********************************************************
 EMERGENCY_ENABLE:
@@ -2415,6 +2541,91 @@ EMERGENCY_DISABLE:
 	RTNI
 
 
+;-----------------------------------------------------------
+;根据通道0转换结果，检测光源状态，并设置光源故障标志位
+;-----------------------------------------------------------
+PROCESS_LIGHT:
+
+	ORIM	FLAG_OCCUPIED,	0001B	;锁定通道0最终结果
+
+	LDI	CHN1_FINAL_RET1,01H
+	SUB	CMP_LIGHT0
+	LDI	CHN1_FINAL_RET2,01H
+	SBC	CMP_LIGHT1
+
+	ANDIM	FLAG_OCCUPIED,	1110B	;释放对通道0最终结果的锁定
+
+	BC	ERROR_LIGHT		;光源故障
+
+	ANDIM	LIGHT_STATE,	1110B	;清除光源故障标志位
+	JMP     PROCESS_LIGHT_END
+
+
+ERROR_LIGHT:
+
+	ORIM	LIGHT_STATE,	0001B	;置光源故障标志位
+
+
+PROCESS_LIGHT_END:
+
+	RTNI
+
+;--------------------------------------------------------------------------------------
+;根据电池电压检测结果、光源检测结果，做相应处理
+;充电回路开路/短路: 红灯灭，黄灯1HZ闪烁
+;光源开路/短路:             黄灯3HZ闪烁
+;放电时间不足:              黄灯常亮
+;--------------------------------------------------------------------------------------
+PROCESS_ALARM:
+
+ALARM_BAT:	
+	LDI	TBR,		0001B	;将0001B载入累加器A中
+	AND	ALARM_STATE 		;判断电池是否处于故障状态
+	BAZ	ALARM_LIGHT		;电池没有故障，则跳转
+
+	ANDIM	PORTC,		1110B	;电池充电回路故障，关闭红灯
+
+	ADI	F_328MS,	01H	;判断是否到了一个新328MS
+	BA0	ALARM_LIGHT		;还未到新的328MS,跳转
+
+	ADIM	CNT_LED_YELLOW,	01H	;328MS已到，计数器加1
+
+	SBI	CNT_LED_YELLOW,	03H
+	BNC	ALARM_LIGHT		;未满1s
+
+	LDI	CNT_LED_YELLOW,	00H	;清零计数器
+	EORIM	PORTE,		0001B	;以1HZ频率翻转黄灯
+
+ALARM_LIGHT:
+	LDI	TBR,		0010B	;将0010B载入累加器A中
+	AND	ALARM_STATE		;判断光源是否处于故障状态
+	BAZ	ALARM_TIME_NOT_ENOUGH	;光源没有故障，则跳转
+
+	ADI	CNT0_328MS,	01H	;判断是否到了一个新328MS
+	BA0	ALARM_TIME_NOT_ENOUGH	;还未到新的328MS,跳转
+
+	EORIM	PORTE,		0001B	;以3HZ频率翻转黄灯
+
+	ANDIM	F_328MS,	1110B	;清除328MS标志
+
+ALARM_TIME_NOT_ENOUGH:
+	LDI	TBR,		0100B	;将0100B载入累加器A中
+	AND	ALARM_STATE		;判断是否出现放电时间不足之故障
+	BAZ	OFF_LED_YELLOW		;没有出现放电时间不足之故障，则跳转
+	
+	ORIM	PORTE,		0001B	;黄灯长亮
+
+OFF_LED_YELLOW:
+	LDA	ALARM_STATE		;如果没有任何故障，则关闭黄灯
+	BNZ	PROCESS_ALARM_END
+	ANDIM	PORTE,		1110B
+
+PROCESS_ALARM_END:
+	RTNI
+
+
+
+	
 	END
 
 
